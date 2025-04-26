4e5ecef3600040883593339fee5bf097744f3ba8
shuya729 <sshuya29@gmail.com>
2025-04-26 13:55:21 +0900

    軽微なレイアウトの修正

--- web/src/components/custom/TextEditor.tsx
+++ web/src/components/custom/TextEditor.tsx
@@ -14,7 +14,7 @@ export const TextEditor = (props: TextEditorProps): JSX.Element => {
 
   if (adjustStatus === 1) {
     return (
-      <div className="flex justify-center flex-auto w-full max-w-2xl min-h-80 h-[calc(100svh-230px)] sm:h-[calc(100svh-294px)] lg:h-[calc(100svh-198px)]">
+      <div className="flex justify-center flex-auto w-full max-w-2xl min-h-80 h-[calc(100svh-214px)] sm:h-[calc(100svh-286px)] lg:h-[calc(100svh-190px)]">
         <div className="w-full border-input rounded-md border bg-transparent px-3 py-2 text-base shadow-xs outline-none cursor-not-allowed opacity-50 space-y-1">
           <Skeleton className="h-4 w-[60%]" />
           <Skeleton className="h-4 w-[90%]" />
@@ -29,7 +29,7 @@ export const TextEditor = (props: TextEditorProps): JSX.Element => {
     );
   }
   return (
-    <div className="flex justify-center flex-auto w-full max-w-2xl min-h-80 h-[calc(100svh-230px)] sm:h-[calc(100svh-294px)] lg:h-[calc(100svh-198px)]">
+    <div className="flex justify-center flex-auto w-full max-w-2xl min-h-80 h-[calc(100svh-214px)] sm:h-[calc(100svh-286px)] lg:h-[calc(100svh-190px)]">
       <Textarea
         placeholder="文章を入力して下さい。&#13;左上のボックスに入力中の文字数が表示され、右上のボタンよりAI文字数調整機能を利用できます。"
         value={text}
