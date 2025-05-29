5de13c1b28d97e8b72af4c9b96de85da8d87dc5b
shuya729 <sshuya29@gmail.com>
2025-05-29 20:41:59 +0900

    余計なコード変更

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  assetsInclude: ["robots.txt"],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
